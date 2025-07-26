66a6a2d252da8efaba90236040ce8c23217f8b84
Damilola Ola <damilolaola@MacBook-Pro.lan>
2025-07-26 01:08:57 -0500

    made changes to homepage

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from DockerHub -Dami's EC2 on Ubuntu 24.04!, I hope you are having a good day"
+    return "Hello from DockerHub -Dami's EC2 on Ubuntu 24.04!, What is going on?"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
